2b761872ae95fe7f0e4486dcb3e99fc4d2b821c0
IvanBersenev <bersenev4dev@gmail.com>
2025-05-18 13:21:48 +0700

    colors

--- src/shared/styles/MyTypography/MyTypography.ts
+++ src/shared/styles/MyTypography/MyTypography.ts
@@ -1,32 +1,40 @@
+import { MyColors } from "../MyColors/MyColors";
+
 export const MyTypography = {
   Helvetica19Medium: {
     fontFamily: "helvetica",
     fontWeight: 400,
     fontSize: 18,
+    color: MyColors.white,
   },
   Helvetica15Thin: {
     fontFamily: "helvetica",
     fontWeight: 300,
     fontSize: 15,
+    color: MyColors.white,
   },
   Helvetica22Normal: {
     fontFamily: "helvetica",
     fontWeight: 400,
     fontSize: 22,
+    color: MyColors.white,
   },
   Helvetica14Normal: {
     fontFamily: "helvetica",
     fontWeight: 500,
     fontSize: 14,
+    color: MyColors.white,
   },
   Helvetica14Medium: {
     fontFamily: "helvetica",
     fontWeight: 400,
     fontSize: 14,
+    color: MyColors.white,
   },
   Helvetica16Normal: {
     fontFamily: "helvetica",
     fontWeight: 380,
     fontSize: 16,
+    color: MyColors.white,
   },
 };
