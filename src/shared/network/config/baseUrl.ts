289609016ca503a037140855d480bc9d7a7f665f
IvanBersenev <bersenev4dev@gmail.com>
2025-06-20 18:03:01 +0700

    урл

--- src/shared/network/config/baseUrl.ts
+++ src/shared/network/config/baseUrl.ts
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:8090";
+// export const baseUrl = "http://localhost:8090";
 // export const baseUrl = "https://sgl-rights-go-backend-1.onrender.com";
-// export const baseUrl = "https://sgl-rights-go-backend-zeg8.onrender.com";
+export const baseUrl = "https://sgl-rights-go-backend-zeg8.onrender.com";
